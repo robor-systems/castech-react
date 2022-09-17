3655c01c7f59a87fbbfa3449f1c2630e81106461
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-18 02:34:03 +0500

    fix: added dynamic rendering

--- src/components/Views/Home/AboutUs/AboutStats.jsx
+++ src/components/Views/Home/AboutUs/AboutStats.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 
 const AboutStats = () => {
+  const stats = [
+    {
+      stat: "100+",
+      title: "IT Professionals",
+    },
+    {
+      stat: "12+",
+      title: "Years of Experience",
+    },
+    {
+      stat: "24+",
+      title: "Projects Delivered",
+    },
+    {
+      stat: "20+",
+      title: "Global Clients",
+    },
+  ];
   return (
-    <div className="w-[312px] h-[260px] md:w-[580px] lg:w-[800px]  xl:w-[1126px] lg:h-[120px] rounded-[8px] bg-[#EE2737] mt-[100px] flex flex-row justify-around md:px-[50px] xl:px-[100px] ">
-      <div className="flex flex-col lg:flex-row lg:w-[500px] justify-around">
-        <div className="py-[24px] ">
-          <p className="text-[39px] text-[#FFFFFF]">100+</p>
-          <p className="text-[14px] text-[#FFFFFF]">IT Professionals</p>
-        </div>
-        <div className="py-[24px]">
-          <p className="text-[39px] text-[#FFFFFF]">12+</p>
-          <p className="text-[14px] text-[#FFFFFF]">Years of Experience</p>
-        </div>
-      </div>
-      <div className="flex flex-col lg:flex-row lg:w-[500px] justify-around">
-        <div className="py-[24px]">
-          <p className="text-[39px] text-[#FFFFFF]">24+</p>
-          <p className="text-[14px] text-[#FFFFFF]">Projects Delivered</p>
-        </div>
-        <div className="py-[24px] ">
-          <p className="text-[39px] text-[#FFFFFF]">20+</p>
-          <p className="text-[14px] text-[#FFFFFF]">Global Clients</p>
-        </div>
+    <div className='py-10   rounded-[8px] bg-[#EE2737] mt-[100px] '>
+      <div className='grid grid-cols-2 md:grid-cols-4 justify-center items-center max-w-4xl mx-auto'>
+        {stats.map(({ stat, title }, i) => (
+          <div className=' justify-self-center' key={i}>
+            <p className='text-[39px] text-[#FFFFFF]'>{stat}</p>
+            <p className='text-[12px] sm:text-[14px] text-[#FFFFFF]'>{title}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
