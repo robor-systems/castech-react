32369c648ec1e0a2e7a5a5d1248baba751849109
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-18 02:36:41 +0500

    fix: removed useMediaQuery + assets links + fixed styling

--- src/components/Views/Home/Career.jsx
+++ src/components/Views/Home/Career.jsx
@@ -1,107 +1,49 @@
+import { Container } from "components/Modules";
+import { CAREER_IMAGE, DOT_DECOR } from "constants/content.constant";
 import React from "react";
-import { useMediaQuery } from "react-responsive";
-const photo =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/career_photo.jpg";
-const dot_Square =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/dots-square.svg";
 
 const Career = () => {
-  const isSm = useMediaQuery({ query: "(max-width: 620px)" });
   return (
-    <section id="career">
-      {isSm ? (
-        <div className="pt-[50px] flex flex-col  gap-[1rem]  justify-between mx-[10px]  px-[22px] ">
-          <div>
-            <div className="text-text-secondary text-[14px] font-semibold  mt-[50px] md:mt-[0px] ">
+    <section id='career' className='my-20'>
+      <Container>
+        <div className='flex gap-x-20 max-w-6xl items-stretch mx-auto md:flex-row flex-col-reverse gap-y-14'>
+          <div className='h-full w-full flex-[1] relative '>
+            <img
+              src={CAREER_IMAGE}
+              alt='Career'
+              className='rounded-[40px] md:rounded-[80px] md:min-h-[350px]'
+            />
+            <img
+              src={DOT_DECOR}
+              alt='decor'
+              className='absolute -bottom-14 -z-10'
+            />
+          </div>
+          <div className='flex-[1] lg:pr-16 '>
+            <div className='text-text-secondary text-[14px] font-semibold'>
               WHY JOIN US
             </div>
-            <div>
-              <h1 className="text-[24px] md:text-[32px] leading-[2rem] md:leading-[2.5rem] mb-[12px] font-semibold w-[18rem] md:w-[36rem]">
-                We value or people and their cultures lorem ipsum.
-              </h1>
+            <div className='text-[24px] md:text-[32px] leading-[2rem] md:leading-[2.5rem] font-semibold my-3'>
+              We value or people and their cultures lorem ipsum.
             </div>
-            <p className="text-text-primary text-[14px] leading-[1.5rem]">
+            <div className='text-text-primary text-[14px] leading-[1.5rem]'>
               We in CASTechnology Inc. believe those employees are asset of
-              organization. We <br /> care them and do whatever is possible to
-              train and retain them. We maintain a <br /> policy of
-              non-discrimination towards all employees and applicants for
-              employment. <br />
-              All aspects of employment with CASTechnology Inc. are governed by
-              merit, <br /> competence, suitability and qualifications, and will
-              not be influenced in any manner
-              <br /> by gender, age, race, color, religion, national origin or
-              disability
-            </p>
-            <p className="text-text-primary text-[14px] leading-[2rem]">
+              organization. We care them and do whatever is possible to train
+              and retain them. We maintain a policy of non-discrimination
+              towards all employees and applicants for employment. All aspects
+              of employment with CASTechnology Inc. are governed by merit,
+              competence, suitability and qualifications, and will not be
+              influenced in any manner by gender, age, race, color, religion,
+              national origin or disability. <br />
               Want to be a part of our team? Send your resumé for future opening
-              at
-            </p>
-            <p className="text-text-secondary text-[14px] hover:underline cursor-pointer font-semibold">
-              admin_hr@CASTechnology.ca
-            </p>
-          </div>
-          <div className="relative ">
-            <img
-              src={photo}
-              alt="groupimage"
-              className="w-[312px] md:w-[496px] h-[239px] md:h-[380px]   rounded-[35px] md:rounded-[80px] relative z-10 "
-            />
-            <div className="absolute left-[-3%] md:left-[-15%] bottom-[-11%] md:bottom-[28%] xl:bottom-[-8%] ">
-              <img
-                src={dot_Square}
-                alt="dots"
-                className="h-[52px] md:h-[auto]"
-              />
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="pt-[100px] flex flex-col md:flex-row md:justify-around gap-[1rem] md:gap-[5rem] justify-between mx-[10px] md:mx-0  px-[22px] md:px-[100px] lg:px-[100px] xl:px-[200px] 3xl:px-[600px]">
-          <div className="relative ">
-            <img
-              src={photo}
-              alt="groupimage"
-              className="w-[312px] md:w-[496px] h-[239px] md:h-[380px]   rounded-[35px] md:rounded-[80px] relative z-10 "
-            />
-            <div className="absolute left-[-3%] md:left-[-12%] bottom-[-11%] md:bottom-[28%] xl:bottom-[-8%] ">
-              <img
-                src={dot_Square}
-                alt="dots"
-                className="h-[52px] md:h-[auto]"
-              />
-            </div>
-          </div>
-          <div>
-            <div className="text-text-secondary text-[14px] font-semibold mb-[10px]  mt-[50px] md:mt-[0px] ">
-              WHY JOIN US
-            </div>
-            <div>
-              <h1 className="text-[24px] md:text-[32px] leading-[2rem] md:leading-[2.5rem] mb-[12px] font-semibold w-[18rem] md:w-[36rem]">
-                We value or people and their cultures lorem ipsum.
-              </h1>
-            </div>
-            <div className="w-[18rem] md:w-[36rem]">
-              <p className="text-text-primary text-[14px] leading-[1.5rem]">
-                We in CASTechnology Inc. believe those employees are asset of
-                organization. We care them and do whatever is possible to train
-                and retain them. We maintain a policy of non-discrimination
-                towards all employees and applicants for employment. All aspects
-                of employment with CASTechnology Inc. are governed by merit,
-                competence, suitability and qualifications, and will not be
-                influenced in any manner by gender, age, race, color, religion,
-                national origin or disability.
-              </p>
-              <p className="text-text-primary text-[14px] leading-[2rem]">
-                Want to be a part of our team? Send your resumé for future
-                opening at
-              </p>
-              <p className="text-text-secondary text-[14px] hover:underline cursor-pointer font-semibold">
+              at{" "}
+              <span className='text-text-secondary text-[14px] hover:underline cursor-pointer  font-semibold'>
                 admin_hr@CASTechnology.ca
-              </p>
+              </span>
             </div>
           </div>
         </div>
-      )}
+      </Container>
     </section>
   );
 };
