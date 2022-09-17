05cd552f55e56ec9054bc65cbbb5d6e5ff18ec24
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-18 02:35:19 +0500

    fix: fixed styling + removed assets constant

--- src/components/Views/Home/Hero.jsx
+++ src/components/Views/Home/Hero.jsx
@@ -1,45 +1,43 @@
+import { Container } from "components/Modules";
+import { DOT_DECOR, HERO_IMAGE } from "constants/content.constant";
 import React from "react";
-import { useNavigate } from "react-router-dom";
-const Hero_image =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/hero-image.jpg";
-const dot_Square =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/dots-square.svg";
+import { Link } from "react-router-dom";
+
 const Hero = () => {
-  const navigate = useNavigate();
   return (
-    <div className="flex flex-col sm:flex-row justify-around 3xl:px-[500px]">
-      <div className="text-center sm:text-left mt-[130px]  mx-[15px] lg:mt-[85px] xl:mt-[204px]  lg:ml-[85px] xl:ml-[188px] ">
-        <div className="flex flex-col items-center">
-          <div className="w-[20rem] lg:w-[25rem] xl:w-[34rem]">
-            <h1 className="text-[32px]  sm:text-[54px] font-bold leading-[2.5rem] sm:leading-[4.5rem] ">
+    <Container className='sm:px-0  px-0'>
+      <div className=' md:flex flex-col sm:flex-row justify-between min-h-[500px]'>
+        <div className='text-center md:text-left flex flex-col justify-center px-10 sm:px-20 md:py-0 py-16'>
+          <div className='max-w-2xl '>
+            <h1 className='text-[32px]  sm:text-[54px] font-bold leading-[2.5rem] sm:leading-[4.5rem] '>
               Lorem Ipsum Dolor <br /> Sit Amet
             </h1>
-            <p className="text-[16px] md:text-[20px] text-text-primary mt-[12px] ">
+            <p className='text-[16px] md:text-[20px] text-text-primary mt-[12px] '>
               Quisque eget erat ornare, tempor dui eu, maximus leo. sagittis
               urna sodales tincidunt.
             </p>
           </div>
+          <div className='mt-[26px] '>
+            <Link
+              to='/contact'
+              className='bg-[#EE2737] inline-block text-center hover:bg-[#DF1D2D] text-white  py-[14px] rounded w-[240px] sm:w-[269px] text-[16px] sm:text-[20px]'
+            >
+              Book Free Consultation
+            </Link>
+          </div>
         </div>
-        <div className="mt-[26px] mb-[60px]">
-          <button
-            onClick={() => navigate("/contact")}
-            className="bg-[#EE2737] hover:bg-[#DF1D2D] text-white  py-[14px] rounded w-[240px] sm:w-[269px] text-[16px] sm:text-[20px]"
-          >
-            Book Free Consultation
-          </button>
+        <div className='h-fit relative'>
+          <img
+            alt='hero_image'
+            src={HERO_IMAGE}
+            className='md:rounded-bl-[80px] w-full md:w-[600px] md:h-[400px]'
+          />
+          <div className='absolute right-0  bottom-[-65px]'>
+            <img src={DOT_DECOR} alt='dots' />
+          </div>
         </div>
       </div>
-      <div className="relative">
-        <img
-          alt="hero_image"
-          src={Hero_image}
-          className=" h-[282px] lg:w-[616px]  lg:h-[483px] sm:rounded-bl-[80px] lg:mt-[35px]"
-        />
-        <div className="absolute right-0 w-[110px] sm:w-[194px]  bottom-[-16%] md:bottom-[-2%]">
-          <img src={dot_Square} alt="dots" />
-        </div>
-      </div>
-    </div>
+    </Container>
   );
 };
 
