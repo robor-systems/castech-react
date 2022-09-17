7a6ffbce207fa8950b06e136aa13f7ca4acc8954
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-18 02:32:50 +0500

    feat: added assets + content constants

---
+++ src/constants/content.constant.js
@@ -0,0 +1,37 @@
+export const LOGO =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/logo/CASTechnology+Inc.png";
+
+export const HERO_IMAGE =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/hero-image.jpg";
+
+export const DOT_DECOR =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/dots-square.svg";
+
+export const CAREER_IMAGE =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/career_photo.jpg";
+
+export const ABOUT_US_IMAGE =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/home-image-2.jpg";
+
+export const COMPANIES = [
+  {
+    alt: "Siemens Healthineers",
+    logo: "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/siemens-healthineers-seeklogo.com.png",
+  },
+  {
+    alt: "Spotify",
+    logo: "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/spotify-seeklogo.com.png",
+  },
+  {
+    alt: "Premier League",
+    logo: "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/premium-league.png",
+  },
+  {
+    alt: "Vortex",
+    logo: "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/vortex.png",
+  },
+  {
+    alt: "Amplitude",
+    logo: "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/amplitude-seeklogo.com.png",
+  },
+];
