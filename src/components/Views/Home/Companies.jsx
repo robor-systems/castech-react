36c7ed62cf62d0c7d53507870b95fa113950e557
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-21 17:20:20 +0500

    fix: refactor code + fixed styling issues

--- src/components/Views/Home/Companies.jsx
+++ src/components/Views/Home/Companies.jsx
@@ -2,21 +2,10 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { useMediaQuery } from "react-responsive";
-
-const siemens_healthineers =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/siemens-healthineers-seeklogo.com.png";
-const spotify =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/spotify-seeklogo.com.png";
-const premium_league =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/premium-league.png";
-const vortex =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/vortex.png";
-const amplitude =
-  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/amplitude-seeklogo.com.png";
+import { COMPANIES } from "constants/content.constant";
+import { Container } from "components/Modules";
 
 const Companies = () => {
-  const isSm = useMediaQuery({ query: "(max-width: 768px)" });
   var settings = {
     dots: true,
     infinite: true,
@@ -31,48 +20,32 @@ const Companies = () => {
     appendDots: (dots) => <div>{dots}</div>,
   };
   return (
-    <div className="flex flex-col text-center mt-[150px]">
-      <p className="text-text-primary font-semibold">
-        Trusted by top-tier companies
-      </p>
-      {isSm ? (
-        <Slider {...settings}>
-          <div className="!flex justify-center mt-[32px]  ">
-            <img src={siemens_healthineers} alt="siemens" />
-          </div>
-          <div className="!flex justify-center mt-[32px] ">
-            <img src={spotify} alt="logo" />
-          </div>
-          <div className="!flex justify-center mt-[32px]">
-            <img src={premium_league} alt="premium" />
-          </div>
-          <div className="!flex justify-center mt-[32px] ">
-            <img src={vortex} alt="vortex" />
-          </div>
-          <div className="!flex justify-center mt-[32px] ">
-            <img src={amplitude} alt="amplitude" />
-          </div>
-        </Slider>
-      ) : (
-        <div className="md:flex md:justify-center">
-          <div className="mx-[38px] my-[38px]">
-            <img src={siemens_healthineers} alt="siemens" />
-          </div>
-          <div className="mx-[38px] my-[38px]">
-            <img src={spotify} alt="logo" />
-          </div>
-          <div className="mx-[38px] my-[38px]">
-            <img src={premium_league} alt="premium" />
-          </div>
-          <div className="mx-[38px] my-[38px]">
-            <img src={vortex} alt="vortex" />
-          </div>
-          <div className="mx-[38px] my-[38px]">
-            <img src={amplitude} alt="amplitude" />
-          </div>
+    <Container className='my-28 sm:my-0 sm:py-20'>
+      <div className='flex flex-col text-center max-w-6xl mx-auto'>
+        <p className='text-text-primary font-semibold mb-10'>
+          Trusted by top-tier companies
+        </p>
+        {/* Slider for <768px screen */}
+        <div className='md:hidden block'>
+          <Slider {...settings}>
+            {COMPANIES.map(({ alt, logo }, i) => (
+              <div className='!flex justify-center mt-[32px]  '>
+                <img src={logo} alt={alt} />
+              </div>
+            ))}
+          </Slider>
         </div>
-      )}
-    </div>
+
+        {/* Static view for laptop screens */}
+        <div className='md:grid grid-cols-3 gap-y-8 justify-center   lg:grid-cols-5 hidden  '>
+          {COMPANIES.map(({ alt, logo }, i) => (
+            <div className='justify-self-center'>
+              <img src={logo} alt={alt} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </Container>
   );
 };
 
