21aa3373c3e4b7294db56bd3f9885c7b9000e815
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-21 17:28:40 +0500

    feat: added content

--- src/constants/content.constant.js
+++ src/constants/content.constant.js
@@ -13,6 +13,24 @@ export const CAREER_IMAGE =
 export const ABOUT_US_IMAGE =
   "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/home-image-2.jpg";
 
+export const SUCCESS_ICON =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/icons/success-icon.svg";
+
+export const TWITTER_ICON =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/Footer-icons/twitter.svg";
+export const LINKEDIN_ICON =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/Footer-icons/linkedin.svg";
+export const FACEBOOK_ICON =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/Footer-icons/facebook.svg";
+export const INSTAGRAM_ICON =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/Footer-icons/instagram.svg";
+export const LOCATION_ICON =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/Footer-icons/location-on.svg";
+export const EMAIL_ICON =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/Footer-icons/email.svg";
+export const PHONE_ICON =
+  "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/Footer-icons/local-phone.svg";
+
 export const COMPANIES = [
   {
     alt: "Siemens Healthineers",
@@ -33,5 +51,48 @@ export const COMPANIES = [
   {
     alt: "Amplitude",
     logo: "https://robor-team.s3.eu-central-1.amazonaws.com/cas-tech/amplitude-seeklogo.com.png",
+  },
+];
+
+export const SERVICES = [
+  {
+    heading: "Cloud Computing",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna.",
+  },
+  {
+    heading: "Cyber Security",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna.",
+  },
+  {
+    heading: "Data Warehousing",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna.",
+  },
+  {
+    heading: "Quality Assurance",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna.",
+  },
+  {
+    heading: "Blockchain",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna.",
+  },
+  {
+    heading: "Advisory",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna.",
+  },
+  {
+    heading: "Data Storage",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna.",
+  },
+  {
+    heading: "IT Offshoring",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna.",
   },
 ];
