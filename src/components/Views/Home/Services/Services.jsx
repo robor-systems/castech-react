eaf4d5764adbec51d49237f92e4ec8c0cf9bddbc
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-21 17:19:00 +0500

    fix: refactor + fixed styling issues

--- src/components/Views/Home/Services/Services.jsx
+++ src/components/Views/Home/Services/Services.jsx
@@ -1,71 +1,30 @@
+import { Container } from "components/Modules";
+import { SERVICES } from "constants/content.constant";
 import React from "react";
 import TechBox from "./TechBox";
 
 const Services = () => {
   return (
-    <section name='service'>
-      <div className='flex flex-col items-center'>
-        <h2 className='text-text-secondary text-[14px] text-center pt-[100px]  mb-[10px] font-semibold'>
-          WHAT WE DO
-        </h2>
-        <h1 className='text-center text-[24px] sm-[32px] font-semibold px-[30px]'>
-          Lorem ipsum dolor sit amet
-        </h1>
-        <p className='text-center text-[16px] text-text-primary mb-[48px] px-[25px] mt-[8px]'>
-          Our technology allows you to provide lorem ipsum dolor sit amet.
-        </p>
+    <section name='service' id='services' className='my-20 sm:my-0 sm:py-20'>
+      <Container>
+        <div className='flex flex-col items-center max-w-6xl mx-auto'>
+          <h2 className='text-text-secondary text-[14px] text-center font-semibold'>
+            WHAT WE DO
+          </h2>
+          <h1 className='text-center text-[24px] sm-[32px] font-semibold px-[30px]'>
+            Lorem ipsum dolor sit amet
+          </h1>
+          <p className='text-center text-[16px] text-text-primary my-3'>
+            Our technology allows you to provide lorem ipsum dolor sit amet.
+          </p>
 
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-6  px-[10px]'>
-          <TechBox
-            heading={"Cloud Computing"}
-            para={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna."
-            }
-          />
-          <TechBox
-            heading={"Cyber Security"}
-            para={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna."
-            }
-          />
-          <TechBox
-            heading={"Data Warehousing"}
-            para={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna."
-            }
-          />
-          <TechBox
-            heading={"Quality Assurance"}
-            para={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna."
-            }
-          />
-          <TechBox
-            heading={"Blockchain"}
-            para={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna."
-            }
-          />
-          <TechBox
-            heading={"Advisory"}
-            para={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna."
-            }
-          />
-          <TechBox
-            heading={"Data Storage"}
-            para={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna."
-            }
-          />
-          <TechBox
-            heading={"IT Offshoring"}
-            para={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex ipsum, rhoncus sed sollicitudin eget, cursus ultricies magna."
-            }
-          />
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-14'>
+            {SERVICES.map(({ heading, details }, i) => (
+              <TechBox key={i} heading={heading} para={details} />
+            ))}
+          </div>
         </div>
-      </div>
+      </Container>
     </section>
   );
 };
--- src/components/Views/Home/Services/TechBox.jsx
+++ src/components/Views/Home/Services/TechBox.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const TechBox = ({ heading, para }) => {
   return (
-    <div className="w-[312px] sm:w-[258px] h-[179px] sm:h-[203px] border-solid border-2 border-[#EEF2F6] rounded-[6px] opacity: 1 p-[22px]">
-      <h1 className="text-[20px] font-semibold">{heading}</h1>
-      <p className="text-text-primary text-[14px] mt-[12px]">{para}</p>
+    <div className='border-solid border-2 border-[#EEF2F6] rounded-[6px] py-8 px-8'>
+      <h1 className='text-[20px] font-semibold'>{heading}</h1>
+      <p className='text-text-primary text-[14px] mt-[12px]'>{para}</p>
     </div>
   );
 };
