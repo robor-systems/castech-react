1170685b2c7c55c2b50d91514023c8a73fd229c9
Ahmad Tufail <52331077+ahmadkhudai@users.noreply.github.com>
2023-01-23 16:06:25 +0500

    fix: sanity client should not crash the app. it should fail quietly

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,8 +1,30 @@
 import sanityClient from "@sanity/client";
 
-export default sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-    dataset: process.env.REACT_APP_SANITY_DATASET,
-    useCdn: true,
-    apiVersion: "2021-03-25",
-});
+let sanityClientInstance = {
+    false_instance: true,
+    fetch: async () => {
+        return [];
+    }
+};
+
+export default (() => {
+    if (!sanityClientInstance.false_instance) {
+        return sanityClientInstance;
+    }
+
+    try {
+
+        sanityClientInstance = sanityClient({
+            projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+            dataset: process.env.REACT_APP_SANITY_DATASET,
+            useCdn: true,
+            apiVersion: '2021-03-25',
+        });
+    } catch (error) {
+        if (process.env.NODE_ENV === 'development') {
+            console.error(error);
+        }
+    }
+
+    return sanityClientInstance;
+})()
